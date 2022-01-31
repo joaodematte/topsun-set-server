927ff48f685925f17e3a0c130d757aaa358c28fa
João Dematte <jgg.dematte@gmail.com>
2022-01-31 11:41:22 -0300

    added 2 inverters

--- src/app/controller/GenerateDiagramaSimplificado.ts
+++ src/app/controller/GenerateDiagramaSimplificado.ts
@@ -28,7 +28,7 @@ class GenerateDiagramaSimplificado {
       fabricanteModulo,
       modeloModulo,
       potenciaModulo,
-      quantidadeInversores,
+      modeloInversor0,
       modeloInversor1,
     } = req.body;
 
@@ -36,20 +36,32 @@ class GenerateDiagramaSimplificado {
       modeloModulo
     );
 
-    const inverterModelname = await repositoryInverter.findOne(modeloInversor1);
+    const inverterModelname0 = await repositoryInverter.findOne(
+      modeloInversor0
+    );
+
+    let inverterModelname1;
+
+    if (modeloInversor1) {
+      inverterModelname1 = await repositoryInverter.findOne(modeloInversor1);
+    }
 
     const manufacturerName = await repositoryManufacturer.findOne(
       fabricanteModulo
     );
 
-    const inversor_modelo_array = inverterModelname.model.split("|");
-    console.log(inversor_modelo_array);
+    const inversor_modelo0_array = inverterModelname0.model.split("|");
+
+    let inversor_modelo1_array;
+    if (modeloInversor1) {
+      inversor_modelo1_array = inverterModelname1.model.split("|");
+    }
 
     const pdfDoc = await PDFDocument.load(
       fs.readFileSync(
         `./${
           process.env.APP_TYPE == "BUILD" ? "dist" : "src"
-        }/utils/simplificado/${tipoLigacao}.pdf`
+        }/utils/simplificado/${modeloInversor1 ? "2" : "1"}/${tipoLigacao}.pdf`
       )
     );
 
@@ -64,127 +76,281 @@ class GenerateDiagramaSimplificado {
 
     const pages = pdfDoc.getPages();
 
-    pages[0].drawText(`${medidor.trim()}`, {
-      x: 400 - customFont.widthOfTextAtSize(medidor, 15) / 2,
-      y: 605,
-      size: 15,
-      font: customFont,
-      color: rgb(0, 0, 0),
-    });
+    if (modeloInversor1) {
+      pages[0].drawText(`${medidor.trim()}`, {
+        x: 407 - customFont.widthOfTextAtSize(medidor, 15) / 2,
+        y: 646,
+        size: 15,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
 
-    pages[0].drawText(`${capacidadeDisjuntor.trim()}A`, {
-      x: 450,
-      y: 534,
-      size: 15,
-      font: customFont,
-      color: rgb(0, 0, 0),
-    });
+      pages[0].drawText(`${capacidadeDisjuntor.trim()}A`, {
+        x: 458,
+        y: 575.5,
+        size: 15,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
 
-    pages[0].drawText(`Inversor ${inversor_modelo_array[0].trim()}`, {
-      x:
-        335 -
-        customFont.widthOfTextAtSize(
-          `Inversor ${inversor_modelo_array[0].trim()}`,
-          15
-        ) /
-          2,
-      y: 268,
-      size: 15,
-      font: customFont,
-      color: rgb(0, 0, 0),
-    });
-
-    if (inversor_modelo_array[1]) {
-      pages[0].drawText(inversor_modelo_array[1].trim(), {
+      pages[0].drawText(`Inversor ${inversor_modelo0_array[0].trim()}`, {
         x:
           335 -
           customFont.widthOfTextAtSize(
-            `${inversor_modelo_array[1].trim()}`,
+            `Inversor ${inversor_modelo0_array[0].trim()}`,
             15
           ) /
             2,
-        y: 252,
+        y: 311,
+        size: 15,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
+
+      if (inversor_modelo0_array[1]) {
+        pages[0].drawText(inversor_modelo0_array[1].trim(), {
+          x:
+            335 -
+            customFont.widthOfTextAtSize(
+              `${inversor_modelo0_array[1].trim()}`,
+              15
+            ) /
+              2,
+          y: 295,
+          size: 15,
+          font: customFont,
+          color: rgb(0, 0, 0),
+        });
+      }
+
+      pages[0].drawText(`Inversor ${inversor_modelo1_array[0].trim()}`, {
+        x:
+          335 -
+          customFont.widthOfTextAtSize(
+            `Inversor ${inversor_modelo1_array[0].trim()}`,
+            15
+          ) /
+            2,
+        y: 170,
+        size: 15,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
+
+      if (inversor_modelo1_array[1]) {
+        pages[0].drawText(inversor_modelo1_array[1].trim(), {
+          x:
+            335 -
+            customFont.widthOfTextAtSize(
+              `${inversor_modelo1_array[1].trim()}`,
+              15
+            ) /
+              2,
+          y: 154,
+          size: 15,
+          font: customFont,
+          color: rgb(0, 0, 0),
+        });
+      }
+
+      pages[0].drawText(
+        `Inversor = ${fixNumber(inverterModelname0.activePower)}W`,
+        {
+          x:
+            825 -
+            customFont.widthOfTextAtSize(
+              `Inversor = ${fixNumber(inverterModelname0.activePower)}`,
+              15
+            ) /
+              2,
+          y: 180,
+          size: 15,
+          font: customFont,
+          color: rgb(0, 0, 0),
+        }
+      );
+
+      pages[0].drawText(
+        `Módulos = ${fixNumber(potenciaModulo * quantidadeModulos)}Wp`,
+        {
+          x:
+            825 -
+            customFont.widthOfTextAtSize(
+              `Módulos = ${fixNumber(potenciaModulo * quantidadeModulos)}`,
+              15
+            ) /
+              2,
+          y: 164,
+          size: 15,
+          font: customFont,
+          color: rgb(0, 0, 0),
+        }
+      );
+
+      pages[0].drawText(`Módulo ${manufacturerName.name.trim()}`, {
+        x:
+          360 -
+          customFont.widthOfTextAtSize(
+            `Módulo ${manufacturerName.name.trim()}`,
+            15
+          ) /
+            2,
+        y: 110,
+        size: 15,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
+
+      pages[0].drawText(`${solarPanelModelName.model.trim()}`, {
+        x:
+          360 -
+          customFont.widthOfTextAtSize(
+            `${solarPanelModelName.model.trim()}`,
+            15
+          ) /
+            2,
+        y: 94,
+        size: 15,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
+
+      pages[0].drawText(`${quantidadeModulos} x ${potenciaModulo}Wp`, {
+        x:
+          360 -
+          customFont.widthOfTextAtSize(
+            `${quantidadeModulos} x ${potenciaModulo}Wp`,
+            15
+          ) /
+            2,
+        y: 78,
+        size: 15,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
+    } else {
+      pages[0].drawText(`${medidor.trim()}`, {
+        x: 400 - customFont.widthOfTextAtSize(medidor, 15) / 2,
+        y: 605,
+        size: 15,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
+
+      pages[0].drawText(`${capacidadeDisjuntor.trim()}A`, {
+        x: 450,
+        y: 534,
+        size: 15,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
+
+      pages[0].drawText(`Inversor ${inversor_modelo0_array[0].trim()}`, {
+        x:
+          335 -
+          customFont.widthOfTextAtSize(
+            `Inversor ${inversor_modelo0_array[0].trim()}`,
+            15
+          ) /
+            2,
+        y: 268,
+        size: 15,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
+
+      if (inversor_modelo0_array[1]) {
+        pages[0].drawText(inversor_modelo0_array[1].trim(), {
+          x:
+            335 -
+            customFont.widthOfTextAtSize(
+              `${inversor_modelo0_array[1].trim()}`,
+              15
+            ) /
+              2,
+          y: 252,
+          size: 15,
+          font: customFont,
+          color: rgb(0, 0, 0),
+        });
+      }
+
+      pages[0].drawText(
+        `Inversor = ${fixNumber(inverterModelname0.activePower)}W`,
+        {
+          x:
+            675 -
+            customFont.widthOfTextAtSize(
+              `Inversor = ${fixNumber(inverterModelname0.activePower)}`,
+              15
+            ) /
+              2,
+          y: 136,
+          size: 15,
+          font: customFont,
+          color: rgb(0, 0, 0),
+        }
+      );
+
+      pages[0].drawText(
+        `Módulos = ${fixNumber(potenciaModulo * quantidadeModulos)}Wp`,
+        {
+          x:
+            675 -
+            customFont.widthOfTextAtSize(
+              `Módulos = ${fixNumber(potenciaModulo * quantidadeModulos)}`,
+              15
+            ) /
+              2,
+          y: 120,
+          size: 15,
+          font: customFont,
+          color: rgb(0, 0, 0),
+        }
+      );
+
+      pages[0].drawText(`Módulo ${manufacturerName.name.trim()}`, {
+        x:
+          335 -
+          customFont.widthOfTextAtSize(
+            `Módulo ${manufacturerName.name.trim()}`,
+            15
+          ) /
+            2,
+        y: 160,
+        size: 15,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
+
+      pages[0].drawText(`${solarPanelModelName.model.trim()}`, {
+        x:
+          335 -
+          customFont.widthOfTextAtSize(
+            `${solarPanelModelName.model.trim()}`,
+            15
+          ) /
+            2,
+        y: 144,
+        size: 15,
+        font: customFont,
+        color: rgb(0, 0, 0),
+      });
+
+      pages[0].drawText(`${quantidadeModulos} x ${potenciaModulo}Wp`, {
+        x:
+          335 -
+          customFont.widthOfTextAtSize(
+            `${quantidadeModulos} x ${potenciaModulo}Wp`,
+            15
+          ) /
+            2,
+        y: 128,
         size: 15,
         font: customFont,
         color: rgb(0, 0, 0),
       });
     }
-
-    pages[0].drawText(
-      `Inversor = ${fixNumber(inverterModelname.activePower)}W`,
-      {
-        x:
-          675 -
-          customFont.widthOfTextAtSize(
-            `Inversor = ${fixNumber(inverterModelname.activePower)}`,
-            15
-          ) /
-            2,
-        y: 136,
-        size: 15,
-        font: customFont,
-        color: rgb(0, 0, 0),
-      }
-    );
-
-    pages[0].drawText(
-      `Módulos = ${fixNumber(potenciaModulo * quantidadeModulos)}Wp`,
-      {
-        x:
-          675 -
-          customFont.widthOfTextAtSize(
-            `Módulos = ${fixNumber(potenciaModulo * quantidadeModulos)}`,
-            15
-          ) /
-            2,
-        y: 120,
-        size: 15,
-        font: customFont,
-        color: rgb(0, 0, 0),
-      }
-    );
-
-    pages[0].drawText(`Módulo ${manufacturerName.name.trim()}`, {
-      x:
-        335 -
-        customFont.widthOfTextAtSize(
-          `Módulo ${manufacturerName.name.trim()}`,
-          15
-        ) /
-          2,
-      y: 160,
-      size: 15,
-      font: customFont,
-      color: rgb(0, 0, 0),
-    });
-
-    pages[0].drawText(`${solarPanelModelName.model.trim()}`, {
-      x:
-        335 -
-        customFont.widthOfTextAtSize(
-          `${solarPanelModelName.model.trim()}`,
-          15
-        ) /
-          2,
-      y: 144,
-      size: 15,
-      font: customFont,
-      color: rgb(0, 0, 0),
-    });
-
-    pages[0].drawText(`${quantidadeModulos} x ${potenciaModulo}Wp`, {
-      x:
-        335 -
-        customFont.widthOfTextAtSize(
-          `${quantidadeModulos} x ${potenciaModulo}Wp`,
-          15
-        ) /
-          2,
-      y: 128,
-      size: 15,
-      font: customFont,
-      color: rgb(0, 0, 0),
-    });
 
     fs.writeFileSync(
       `./${
