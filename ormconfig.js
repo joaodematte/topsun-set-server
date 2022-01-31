33d33408b5ee574b2ae87a955ca05f42005b7ffd
João Dematte <demattejoao@gmail.com>
2022-01-31 02:20:02 -0300

    change typeorm apth

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
-  entities: ["dist/app/model/**/*.ts"],
-  migrations: ["dist/database/migration/**/*.ts"],
+  entities: ["dist/app/model/*{.ts,.js}"],
+  migrations: ["dist/database/migration/*{.ts,.js}"],
   cli: {
     migrationsDir: "dist/database/migration",
   },
