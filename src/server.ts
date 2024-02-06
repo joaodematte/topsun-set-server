c89c2633699a4902aa2fdece1c0197cace124a3b
joão dematte <jgdematte@gmail.com>
2024-02-06 13:56:30 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -9,12 +9,24 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({
+app.use(
+  cors({
+    origin: "*",
     credentials: true,
     preflightContinue: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE', 'OPTIONS'],
-    origin: true
-}));
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  })
+);
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  next();
+});
 
 app.use(router);
 
