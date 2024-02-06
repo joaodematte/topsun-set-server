253434e2d2cfd680a568085710e78424fe0d0a82
joão dematte <jgdematte@gmail.com>
2024-02-06 14:33:09 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -11,16 +11,6 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-
-  next();
-});
-
 app.use(router);
 
 const port = process.env.PORT || 3333;
